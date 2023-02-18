ff46e7a59c1d9597be6a15fa3dde2d418a9a5e37
Gavin <waltersg298@gmail.com>
2023-02-17 18:24:50 -0700

    added more tests

--- basic-testing-frontend/src/math.test.js
+++ basic-testing-frontend/src/math.test.js
@@ -16,3 +16,26 @@ it('should summarize all number values in an array', () => {
   expect(result).toBe(expectedResult); 
 });
 
+it('should yield NaN if a least one invalid number is provided', () => {
+  // arrange  
+  const inputs = ['hi',1,2,3];
+
+  // act 
+  const result = add(inputs); 
+
+  // assert 
+  expect(result).toBeNaN(); 
+});
+
+it('should yield a correct sum if an array of string values is provided', () => {
+  const numbers = ['1','2'];
+
+  const result = add(numbers); 
+
+  const expectedResult = numbers.reduce(
+    (prevValue, currentValue) => +prevValue + +currentValue, 0 
+  );
+  expect(result).toBe(expectedResult);
+
+}); 
+
