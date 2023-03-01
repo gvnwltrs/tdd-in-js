3ec375e8b538e434b846b571b112bfe77a210f88
Gavin <waltersg298@gmail.com>
2023-03-01 07:31:32 -0700

    updated test watch and added test suites to validation

--- basic-testing-frontend/src/util/validation.test.js
+++ basic-testing-frontend/src/util/validation.test.js
@@ -1,40 +1,47 @@
-import {it, expect} from 'vitest';
+import {it, expect, describe} from 'vitest';
 
 import { validateStringNotEmpty, validateNumber} from './validation';
 
-it('should take a string type', () => {
-  const input = 'string';
+describe('validateStringNotEmpty()', () => {
 
-  const result = validateStringNotEmpty(input); 
+  it('should take a string type', () => {
+    const input = 'string';
+  
+    const result = validateStringNotEmpty(input); 
+  
+    expect(result).toBeTypeOf('string');
+  }); 
 
-  expect(result).toBeTypeOf('string');
-}); 
+  it('should throw error for empty string', () => {
+    const input = '';
+  
+    const result = () => {
+      validateStringNotEmpty(input); 
+    };
+  
+    expect(result).toThrowError(); 
+  });
 
-it('should throw error for empty string', () => {
-  const input = '';
+});
 
-  const result = () => {
-    validateStringNotEmpty(input); 
+describe('validateNumber()', () => {
+
+  it('should take a number type', () => {
+    const input = 1; 
+  
+    const result = validateNumber(input);
+  
+    expect(result).toBeTypeOf('number');
+  }); 
+  
+  it('should throw error for NaN', () => {
+    const input = 'NaN';
+  
+    const result = () => {
+      validateNumber(input);
   };
+  
+    expect(result).toThrowError();
+  });
 
-  expect(result).toThrowError(); 
 });
-
-it('should take a number type', () => {
-  const input = 1; 
-
-  const result = validateNumber(input);
-
-  expect(result).toBeTypeOf('number');
-}); 
-
-it('should throw error for NaN', () => {
-  const input = 'NaN';
-
-  const result = () => {
-    validateNumber(input);
-};
-
-  expect(result).toThrowError();
-});
-
